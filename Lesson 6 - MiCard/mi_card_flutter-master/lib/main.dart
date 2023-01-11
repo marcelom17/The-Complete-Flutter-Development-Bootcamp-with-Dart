91a17429ed7d869e1fa9c1416d52d21d033643a5
Marcelo Macedo <marcelomacedodev@gmail.com>
2023-01-11 23:31:49 +0000

    [NEW] Full Section 6 with complete Business Card, with columns, cards with list tile, Custom fonts + material icons

--- Lesson 6 - MiCard/mi_card_flutter-master/lib/main.dart
+++ Lesson 6 - MiCard/mi_card_flutter-master/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,19 +15,74 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            //crossAxisAlignment: CrossAxisAlignment.center,
             //mainAxisSize: MainAxisSize.min,
             //verticalDirection: VerticalDirection.up,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                //margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10.0),
-                color: Colors.red,
-                child: Text("Container 1"),
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('assets/rock.png'),
               ),
+              Text(
+                'Rock',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'Rockstar Flutter Developer',
+                style: TextStyle(
+                    fontSize: 22.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'SourceSansPro'),
+              ),
+              SizedBox(
+                height: 40.0,
+                width: 200.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade600,
+                  ),
+                  title: Text(
+                    "+123 123 456 789",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.mail,
+                    color: Colors.teal.shade600,
+                  ),
+                  title: Text(
+                    "flutter_rockstar_dev@email.com",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
